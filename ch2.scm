cc75ccc423786794e3f88631f57f990374562e07
sterchois <sterchois@protonmail.com>
2018-07-18 16:22:41 +0800

    ...

--- ch2.scm
+++ ch2.scm
@@ -67,8 +67,7 @@
 
 
 
-(define (empty-env)
-  '())
+(define empty-env '())
 
 (define (is-empty-env? env)
   (null? env))
@@ -78,9 +77,13 @@
     ((and (pair? (car env)) (symbol? (caar env))) #f)
     (else (is-empty-env? (cdr env)))))
 
+
 (define (has-binding? env var)
-  (let ((variable value))
-    ()))
+  (cond ((null? env) #f)
+    (else (let ((saved-var (cadr env))
+              (subEnv (cdr env)))
+      (cond ((eqv? saved-var var) #t)
+        (else (has-binding? subEnv var))))))
 
 (define (extended-env var val env)
   (cons (cons var val) env))
@@ -94,9 +97,98 @@
         (else (apply-env saved-env search-var)))))))
 
 
+        ;;;;;;;;;;   (extended-env 'x 10 empty-env)
+
 (define report-no-binding-found
   (lambda (search-var)
     (eopl:error ’apply-env "No binding for ~s" search-var)))
 (define report-invalid-env
   (lambda (env)
     (eopl:error ’apply-env "Bad environment: ~s" env)))
+
+(define empty-env* '(()()))
+
+(define (is-empty-env*? env)
+  (and (null? (car env)) (null? (cadr env) (null? (cddr empty-env)))))
+
+(define (extended-env** vars vals env)
+  (cond ((null? vars) env)
+    (else (list (append (car env) vars) (append (cadr env) vals)))))
+
+(define (extended-env* vars vals env)
+  (cond ((null? vars) env)
+    (else (extended-env* (cdr vars) (cdr vals) (cons (cons (car vars) (car vals)) env)))))
+
+
+    ;;todo buggycode, 只能找到最早bounded值
+(define (apply-env* env search-var)
+  (cond ((is-empty-env*? env) #f)
+    (else (let ((saved-var (caar env))
+                (saved-val (caadr env))
+                (next-env (list (cdar env) (cdadr env))))
+      (cond ((eqv? saved-var search-var) saved-val)
+        (else (apply-env* next-env search-var)))))))
+
+(define (not-empty-env? env)
+  (not (is-empty-env? env)))
+
+
+
+(define-datatype program program?
+  (a-program (exp1 expression?)))
+
+(define-datatype expression expression?
+  (const-exp (num number?))
+  (diff-exp (exp1 expression?) (exp2 expression?))
+  (zero?-exp (exp1 expression?))
+  (if-exp (exp1 expression?) (exp2 expression?) (exp3 expression?))
+  (var-exp (var symbol?))
+  (let-exp (var symbol?) (exp1 expression?) (body expression?))
+  (list-exp (exp1 list-exp?)))
+
+(define-datatype list-exp list-exp?
+  (consExp (ele expression?) (lst list?))
+  (carExp (lst list?))
+  (cdrExp (lst list?))
+  (nullExp (lst list?)))
+
+(define init-env
+  (lambda () (extended-env 'i 1
+                (extended-env 'v 5
+                  (extended-env 'x 10)))))
+
+
+(define-datatype expval expval?
+  (num-val (num number?))
+  (bool-val (bool boolean?))
+  (lst-val (lst list?)))
+
+(define expval->num
+  (lambda (val)
+  (cases expval val
+  (num-val (num) num)
+  (else (report-expval-extractor-error ’num val)))))
+
+(define expval->bool
+  (lambda (val)
+  (cases expval val
+  (bool-val (bool) bool)
+  (else (report-expval-extractor-error ’bool val)))))
+
+(define (expval->list val)
+  (cases expval val
+    (lst-val (lst) lst)
+    (else (report-expval-extractor-error ’list val))))
+
+
+
+
+
+
+
+
+
+
+
+
+
